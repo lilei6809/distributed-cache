16229764dea37d9d11c05487108f7f95450f7fda
lilei6809 <lilei6809@gmail.com>
2024-07-16 14:30:49 +0100

    config management

--- cache-service/src/main/java/com/lanlan/cache/CacheApplication.java
+++ cache-service/src/main/java/com/lanlan/cache/CacheApplication.java
@@ -2,8 +2,10 @@ package com.lanlan.cache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CacheApplication {
 
     public static void main(String[] args) {
