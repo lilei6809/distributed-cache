35cfbf042675b629e7923cb56a74613d5dc2af1f
lilei6809 <lilei6809@gmail.com>
2024-07-17 22:04:56 +0100

    cache-service ci/cd

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -2,9 +2,9 @@
 set -xe
 
 
-# Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://cache-service-stack-webappdeploymentbucket-bywaizukrxve/cache-service-1.0-SNAPSHOT.jar /usr/local/tomcat9/webapps/cache-service-1.0-SNAPSHOT.jar
-
-
-# Ensure the ownership permissions are correct.
-chown -R tomcat:tomcat /usr/local/tomcat9/webapps
+## Copy war file from S3 bucket to tomcat webapp folder
+#aws s3 cp s3://cache-service-stack-webappdeploymentbucket-bywaizukrxve/cache-service-1.0-SNAPSHOT.jar /usr/local/tomcat9/webapps/cache-service-1.0-SNAPSHOT.jar
+#
+#
+## Ensure the ownership permissions are correct.
+#chown -R tomcat:tomcat /usr/local/tomcat9/webapps
